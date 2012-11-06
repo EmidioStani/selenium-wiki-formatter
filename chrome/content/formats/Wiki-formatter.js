3c810d498e933288a1ae44c7809e13d033230e0a
emidiostani <emidiostani@gmail.com>
2012-11-06 17:54:48 +0100

    improved jslint validation

--- chrome/content/formats/Wiki-formatter.js
+++ chrome/content/formats/Wiki-formatter.js
@@ -20,132 +20,144 @@ History:
 */
 
 var wiki = {
-confluence:{
-  start_table: "",
-  end_table: "",
-  header: "||Command||Target||Value||\n",
-  header_row:"||#",
-  start_row: "| ",
-  end_row: " |\n",
-  separator: " | ",
-},
-dokuwiki:{
-  start_table: "",
-  end_table: "",
-  header: "^ Command ^ Target ^ Value ^\n",
-  header_row:"^ # ",
-  start_row: "| ",
-  end_row: " |\n",
-  separator: " | ",
-},
-mediawiki:{
-  start_table: "{|border=\"1\" \n",
-  end_table: "|}",
-  header: "!Command\n!Target\n!Value\n",
-  header_row:"!#\n",
-  start_row: "|-\n|",
-  end_row: "\n",
-  separator: "\n|",
-},
-moinmoin:{
-  start_table: "",
-  end_table: "",
-  header: "||\'\'\'Command\'\'\'||\'\'\'Target\'\'\'||\'\'\'Value\'\'\'||\n",
-  header_row:"||\'\'\'#\'\'\'",
-  start_row: "||",
-  end_row: "||\n",
-  separator: "||",
-},
-textile:{
-  start_table: "",
-  end_table: "",
-  header: "|_. Command|_. Target|_. Value|\n",
-  header_row:"|_. #",
-  start_row: "|",
-  end_row: "|\n",
-  separator: "|",
-},
-trac:{
-  start_table: "",
-  end_table: "",
-  header: "||= Command =||=  Target =||=  Value =||\n",
-  header_row:"||= # =",
-  start_row: "||",
-  end_row: "||\n",
-  separator: "||",
-},
-twiki:{
-  start_table: "",
-  end_table: "",
-  header: "|  *Command*  |  *Target*  |  *Value*  |\n",
-  header_row:"| *#*",
-  start_row: "|  ",
-  end_row: "  |\n",
-  separator: "  |  ",
-},
-xwiki:{
-  start_table: "",
-  end_table: "",
-  header: "|=Command|=Target|=Value\n",
-  header_row:"|=#",
-  start_row: "|",
-  end_row: "\n",
-  separator: "|",
-}
-};
+		confluence: {
+			start_table: "",
+			end_table: "",
+			header: "||Command||Target||Value||\n",
+			header_row: "||#",
+			start_row: "| ",
+			end_row: " |\n",
+			separator: " | "
+		},
+		dokuwiki: {
+			start_table: "",
+			end_table: "",
+			header: "^ Command ^ Target ^ Value ^\n",
+			header_row: "^ # ",
+			start_row: "| ",
+			end_row: " |\n",
+			separator: " | "
+		},
+		mediawiki: {
+			start_table: "{|border=\"1\" \n",
+			end_table: "|}",
+			header: "!Command\n!Target\n!Value\n",
+			header_row: "!#\n",
+			start_row: "|-\n|",
+			end_row: "\n",
+			separator: "\n|"
+		},
+		moinmoin: {
+			start_table: "",
+			end_table: "",
+			header: "||\'\'\'Command\'\'\'||\'\'\'Target\'\'\'||\'\'\'Value\'\'\'||\n",
+			header_row: "||\'\'\'#\'\'\'",
+			start_row: "||",
+			end_row: "||\n",
+			separator: "||"
+		},
+		textile: {
+			start_table: "",
+			end_table: "",
+			header: "|_. Command|_. Target|_. Value|\n",
+			header_row: "|_. #",
+			start_row: "|",
+			end_row: "|\n",
+			separator: "|"
+		},
+		trac: {
+			start_table: "",
+			end_table: "",
+			header: "||= Command =||=  Target =||=  Value =||\n",
+			header_row: "||= # =",
+			start_row: "||",
+			end_row: "||\n",
+			separator: "||"
+		},
+		twiki: {
+			start_table: "",
+			end_table: "",
+			header: "|  *Command*  |  *Target*  |  *Value*  |\n",
+			header_row: "| *#*",
+			start_row: "|  ",
+			end_row: "  |\n",
+			separator: "  |  "
+		},
+		xwiki: {
+			start_table: "",
+			end_table: "",
+			header: "|=Command|=Target|=Value\n",
+			header_row: "|=#",
+			start_row: "|",
+			end_row: "\n",
+			separator: "|"
+		}
+	};
+
+var options = {wiki: 'confluence', row: "false"};
 
 function formatCommands(commands) {
-  var result = '';
-  var start_table =  wiki[options['wiki']]["start_table"];
-  var end_table =  wiki[options['wiki']]["end_table"];
-  var header = wiki[options['wiki']]["header"];
-  var header_row = wiki[options['wiki']]["header_row"];
-  var start_row = wiki[options['wiki']]["start_row"];
-  var end_row = wiki[options['wiki']]["end_row"];
-  var sep = wiki[options['wiki']]["separator"];
-  var row_num = '';
-  for (var i = 0; i < commands.length; i++) {
-    var command = commands[i];
-    if('true' == options.row)
-            row_num= i+sep;
-    if (command.type == 'command') {
-      result += start_row + row_num+ command.command + sep + command.target + sep + command.value + end_row;
-    }
-  }
-    if('true' == options.row)
-         header = header_row+header;
-    result = start_table + header + result + end_table;
-  return result;
+	var result = '',
+		start_table =  wiki[options.wiki].start_table,
+		end_table =  wiki[options.wiki].end_table,
+		header = wiki[options.wiki].header,
+		header_row = wiki[options.wiki].header_row,
+		start_row = wiki[options.wiki].start_row,
+		end_row = wiki[options.wiki].end_row,
+		sep = wiki[options.wiki].separator,
+		row_num = '',
+		i,
+		command;
+	for (i = 0; i < commands.length; i = i + 1) {
+		command = commands[i];
+		if ('true' === options.row) {
+			row_num = i + sep;
+		}
+		if (command.type === 'command') {
+			result += start_row + row_num + command.command + sep + command.target + sep + command.value + end_row;
+		}
+	}
+	if ('true' === options.row) {
+		header = header_row + header;
+	}
+	result = start_table + header + result + end_table;
+	return result;
 }
 
 function parse(testCase, source) {
-  var doc = source;
-  var commands = [];
-  var start_row = wiki[options['wiki']]["start_row"];
-  var sep = wiki[options['wiki']]["separator"];
-  while (doc.length > 0) {
-    var line = /(.*)(\r\n|[\r\n])?/.exec(doc);
-    var line2 = line[1].slice(start_row.length);
-    var array = line2.split(sep);
-    if (array.length >= 3) {
-      var command = new Command();
-      command.command = array[0];
-      command.target = array[1];
-      command.value = array[2];
-      commands.push(command);
-    }
-    doc = doc.substr(line[0].length);
-  }
-  testCase.setCommands(commands);
+	var doc = source,
+		commands = [],
+		start_row = wiki[options.wiki].start_row,
+		sep = wiki[options.wiki].separator,
+		line,
+		line2,
+		array,
+		command,
+		i;
+	if (doc.length > 0) {
+		line = doc.split(/\r?\n/);
+
+		for (i = 0; i < line.length; i = i + 1) {
+			line2 = line[i].slice(start_row.length);
+			array = line2.split(sep);
+			if (array.length >= 3) {
+				command = new Command();
+				command.command = array[0];
+				command.target = array[1];
+				command.value = array[2];
+				commands.push(command);
+			}
+		}
+	}
+
+	testCase.setCommands(commands);
 }
 
 function format(testCase, name) {
-  return formatCommands(testCase.commands);
+	return formatCommands(testCase.commands);
 }
 
-options = {wiki: 'confluence', row:"false"};
-
-configForm ='<description>Choose a wiki syntax:</description>' +
+var configForm = '<description>Choose a wiki syntax:</description>' +
 			'<menulist id="options_wiki"><menupopup>' +
 			'<menuitem label="Confluence" value="confluence"/>' +
 			'<menuitem label="DokuWiki" value="dokuwiki"/>' +
@@ -154,7 +166,7 @@ configForm ='<description>Choose a wiki syntax:</description>' +
 			'<menuitem label="Textile" value="textile"/>' +
 			'<menuitem label="Trac" value="trac"/>' +
 			'<menuitem label="Twiki" value="twiki"/>' +
-			'<menuitem label="Xwiki" value="xwiki"/>'+
-			'</menupopup></menulist>'+
+			'<menuitem label="Xwiki" value="xwiki"/>' +
+			'</menupopup></menulist>' +
 			'<separator class="groove"/>' +
 			'<checkbox id="options_row" label="Row number"/>';
